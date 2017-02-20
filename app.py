2f7c19946ef73f75e7817c4a6fd33995ab4d98d3
Ubuntu <ubuntu@ip-172-31-20-230.us-west-2.compute.internal>
2017-02-21 08:27:54 +0900

    add main function

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import json
 from flask import Flask
@@ -41,3 +42,11 @@ def makeEchoResponse(req):
 		"displayText": speech,
 		"source": "apiai-echo-sample"
 	}
+
+
+if __name__ == '__main__':
+	port = int(os.getenv('PORT', 5000))
+
+	print("Starting app on port %d" % port)
+
+	app.run(debug=False, port=port, host='0.0.0.0')
